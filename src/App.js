0cf9700846b4d5502d918887b71eb19878c6ed1f
SunShineOneX <SunShineOneX@gmail.com>
2020-07-11 17:15:34 -0400

    pushing first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Card from './components/hobbies';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Card 
+        hobby = "Programming"
+        img = "blahblah.url"
+        details = "Giving computers instructions about what they should do next"
+      />
+       <Card 
+        hobby = "Gaming"
+        img = "blahblah.url"
+        details = "Clapping cheeks and taking names in simulated digital warzones"
+      />
+       <Card 
+        hobby = "Chilling"
+        img = "blahblah.url"
+        details = "Watching Forensic Files, movies, and relaxing in general"
+      />
     </div>
   );
 }
